26f752ba57926813873acab7610b9b9a2fc7c8bc
Shaman <pieas.asif4@gmail.com>
2023-03-21 01:03:53 +0600

    Tenant Service Update

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -10,4 +10,8 @@ abstract class Env {
   static const radisPort = _Env.radisPort;
   static const radisUser = _Env.radisUser;
   static const radisPass = _Env.radisPass;
+  static const secret = _Env.secret;
+  static const algorithm = _Env.algorithm;
+  static const accessTokenExpMin = _Env.accessTokenExpMin;
+  static const refreshTokenExpDays = _Env.refreshTokenExpDays;
 }
